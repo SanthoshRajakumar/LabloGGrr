b62678bf7d71244201ae906d5d96778cad07b73c
Johan5434 <eliassonjohan1@gmail.com>
2024-09-19 14:30:01 +0200

    student login page

---
+++ student_login.php
@@ -0,0 +1,14 @@
+
+
+
+<!DOCTYPE html> 
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Student login</title>
+<body>
+    <h1> Student login</h1>
+<body>
+</head>
+
+</html>
