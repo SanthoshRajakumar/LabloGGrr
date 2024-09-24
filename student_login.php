a230b252c4774c13869e5f84818f7dde711a1fb4
Johan5434 <eliassonjohan1@gmail.com>
2024-09-24 09:56:18 +0200

    strongk

--- student_login.php
+++
@@ -1,8 +0,0 @@
-
-<h2>Login</h2>
-<form action="student_choice.php" method="POST">
-    Email: <input type="email" name="email" required/><br />
-    Password: <input type="password" name="lösen" required /><br />
-    <input type="submit" value="Login"/>
-</form>
-
