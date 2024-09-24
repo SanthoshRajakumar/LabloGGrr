f182a3a05885a84c978aa73541a5e5ce3c2c84e3
elsarosenblad <elsa.rosenblads@gmail.com>
2024-09-24 11:24:16 +0200

    .

--- admin_page.php
+++ admin_page.php
@@ -8,13 +8,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin Page</title>
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
 
-<header class="site-header">
-    <h1 class="logo">LabLogger</h1>
+<header>
+    <h1 class="logo">LabLoGGr</h1>
 </header>
 
 <h2 style="font-size: 50px">Admin page</h2>
