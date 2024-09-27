dc26b1c29ed3524aaba5a0aae5bd8414a74aac6e
elsarosenblad <elsa.rosenblads@gmail.com>
2024-09-27 11:38:49 +0200

    Uptade stylesheet and index.php

--- index.php
+++ index.php
@@ -2,7 +2,10 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>LabLoGGr</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <h1>Information Management Systems 1DL471</h1>
