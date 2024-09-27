7f91cae56465f6f584663dde59a7de56b83aad27
elsarosenblad <elsa.rosenblads@gmail.com>
2024-09-27 11:45:51 +0200

    styleing index.php

--- index.php
+++ index.php
@@ -7,6 +7,44 @@
     <title>LabLoGGr</title>
     <link rel="stylesheet" href="style.css">
 </head>
+
+<header>
+    <form action="index.php" method="POST">
+        <input type="submit" value="HOME" class="button"/>
+    </form>
+
+    <form action="about.php" method="POST">
+        <input type="submit" value="ABOUT" class="button"/>
+    </form>
+
+    <h1>LabLoGGr</h1>
+
+    <form action="faq.php" method="POST">
+        <input type="submit" value="FAQ" class="button"/>
+    </form>
+
+    <form action="contact.php" method="POST">
+        <input type="submit" value="CONTACT" class="button"/>
+    </form>
+
+</header>
+
+<main>
+<div class="div1">
+    <div class="box" id="box1">
+        <img src="images/PastedGraphic-1.png" alt="Image 1">
+        <p>Lead students directly to what they need, so students can dive right into learning with the exact right equipment in hand.</p>
+    </div>
+    <div class="box">
+        <img src="images/PastedGraphic-2.png" alt="Image 2">
+        <p>Maximize hands-on learning with more time on labs and tasks. Our product ensures students spend their session time there it should.</p>
+    </div>
+    <div class="box">
+        <img src="images/PastedGraphic-3.png" alt="Image 3">
+        <p>Our system keeps materials organized and minimizes material management, so teachers can focus on educating. </p>
+    </div>
+</div>
+</main>
 <body>
     <h1>Information Management Systems 1DL471</h1>
 
