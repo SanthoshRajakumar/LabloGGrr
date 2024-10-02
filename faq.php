3f5b380bc406cc8a24dd31a79de7ab362c2df40f
elsarosenblad <elsa.rosenblads@gmail.com>
2024-10-02 10:14:27 +0200

    Added new styling to all the

--- faq.php
+++ faq.php
@@ -3,55 +3,63 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>FAQ</title>
-    <link rel="stylesheet" href="style.css">
+    <title>Page</title>
+    <!-- Link to your CSS file -->
+    <link rel="stylesheet" href="style_.css">
+    <!-- Include jQuery library -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 </head>
-
-<header>
-    <form action="index.php" method="POST">
-        <input type="submit" value="HOME" class="button"/>
-    </form>
-
-    <form action="about.php" method="POST">
-        <input type="submit" value="ABOUT" class="button"/>
-    </form>
-
-    <h1>LabLoGGr</h1>
-
-    <form action="faq.php" method="POST">
-        <input type="submit" value="FAQ" class="button"/>
-    </form>
-
-    <form action="contact.php" method="POST">
-        <input type="submit" value="CONTACT" class="button"/>
-    </form>
-
-</header>
-
 <body>
 
-<main>
+
+<div class="sample-header">
+  <div class="sample-header-section">
+    <h1>LabLoGGr</h1>
+    <h2></h2>
+  </div>
+</div>
+
+
+<!-- Content Section -->
+<div class="sample-section-wrap">
+  <div class="sample-section">
+
+  <header>
+<form action="index.php" method="GET">
+      <button type="submit" class="button">HOME</button>
+    </form>
+    <form action="about.php" method="GET">
+      <button type="submit" class="button">ABOUT</button>
+    </form>
+    <form action="faq.php" method="GET">
+      <button type="submit" class="button">FAQ</button>
+    </form>
+    <form action="contact.php" method="GET">
+      <button type="submit" class="button">CONTACT</button>
+    </form>
+</header>
+
 <div class="div1">
-    <div class="text_blocks">
-    <p class="bold">How do you make the inventory tables? What do you need from me to get started?</p>
+  <div class="text_blocks">
+    <h3>How do you make the inventory tables? What do you need from me to get started?</h3>
         <p>We take the list with names of all the shelfs in your inventory (in .csv format) and upload them to our system. 
         The inventory are then displayed in our system and you can start creating courses and sessions with different access. 
         We also put in a bit of manual work to make sure the storage is just as you want it!</p>
     </div>
     <div class="text_blocks">
-        <p class="bold">What industries do you create the inventory displays for?</p>
+        <h3>What industries do you create the inventory displays for?</h3>
         <p>We create inventory displays for all educational laboratories; university, healthcare, reseach and more! </p>
     </div>
 </div>
 
 <div class="div1">
     <div class="text_blocks">
-    <p class="bold">How much does it cost?</p>
+    <h3>How much does it cost?</h3>
         <p>Our pricing is dependant upon the size of your inventory, and also the solutions that you’re interested in implementing. 
         For a price quote, visit our contact page and get in touch with us. </p>
     </div>
     <div class="text_blocks">
-    <p class="bold">How long do the inventory take to set-up?</p>
+    <h3>How long do the inventory take to set-up?</h3>
         <p>The time it takes to set-up your inventory depends on which features you wish to implement, your existing stucture of the storage, etc. 
         The inventory table and amount display on their own are typically straightforward and quick to set-up. 
         For any additional features you select, this time may increase - but it’s hard to give an exact number as it will be assessed on a case-by-case basis.</p>
@@ -60,16 +68,22 @@
 
 <div class="div1">
     <div class="text_blocks">
-    <p class="bold">Can I customize the design of my inventory?</p>
+    <h3>Can I customize the design of my inventory?</h3>
         <p>The look and feel of the inventory table as a whole (interface, fonts, etc.) cannot be changed. 
         However, you can customize the what you want to be written and displayed. Get in touch with us and we will explain more!</p>
     </div>
     <div class="text_blocks">
-    <p class="bold"> Could not find the answers you were looking for?</p>
+    <h3> Could not find the answers you were looking for?</h3>
         <p>Do not hesitate to contact us. You find our contact information in the top menu.</p>
     </div>
 </div>
 
+<div class="footer">
+    <h4> &copy; 2024 LabbLoGGr | Privacy policy | Terms & Condition </h4>
+</div>
 
-</main>
+<script src="java.js">
+</script>
+
 </body>
+</html>
