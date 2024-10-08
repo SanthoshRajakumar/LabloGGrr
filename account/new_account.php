53c4f5254f16b9d45b090c8cb06d6e6fbb77faf5
smosebach <sara_1022@hotmail.com>
2024-10-08 13:28:55 +0200

    admin stuff

--- account/new_account.php
+++ account/new_account.php
@@ -4,6 +4,8 @@
 # rooms input (double select??)
 # sending info to new users email
 
+# Ta bort role???
+
 
 #session_set_cookie_params([
 #    'lifetime' => 0,
@@ -22,13 +24,10 @@ $sql = "SELECT ID, RoleType FROM Roles";
 $result = $link->query($sql);
 ?>
 
-
-
-<form action="create_account.php" method="POST">
+<form action="./backend/create_account.php" method="POST">
     Firstname: <input type="text" id="fname" name="fname" required/><br/>
     Lastname: <input type="text" id="lname" name="lname" required/><br/>
     Email: <input type="email" id="email" name="email" required/><br/>
-    Social Security Number: <input type="text" id="ssn" name="ssn" minlength="13" maxlength="13" placeholder="YYYYMMDD-XXXX" required/><br/>
     Role: <select name="roleid" required>
         <option value="" disabled selected hidden>Select a role</option>
         <?php
@@ -42,28 +41,9 @@ $result = $link->query($sql);
     <input type="submit" value="Create account"/>
 </form>
 
-<?php
-if (isset($_SESSION['message'])) {
-    echo "yaaahooooo";
-    echo "<p>" . $_SESSION['message'] . "</p>"; // Display the message
-    unset($_SESSION['message']); // Clear the message after displaying
-}
-?>
-
 <script>
     const fnameInput = document.getElementById('fname');
     const lnameInput = document.getElementById('lname');
-    const ssnInput = document.getElementById('ssn');
-
-    // Function to automatically format SSN input
-    function formatSSN(SSN) {
-        const formattedSSN = SSN.replace(/\D/g, ''); // Remove non-numeric characters
-        if (formattedSSN.length > 8) {
-            return formattedSSN.slice(0, 8) + '-' + formattedSSN.slice(8, 12); // Format as YYYYMMDD-XXXX
-        } else {
-            return formattedSSN;
-        }
-    }
 
     // Function to automatically format name input
     function formatName(name){
@@ -76,24 +56,6 @@ if (isset($_SESSION['message'])) {
             })
             .join('');
     }
-
-    // Event listener to update the ssn field as the user types
-    ssnInput.addEventListener('input', function () {
-        const cursorPosition = ssnInput.selectionStart; // Save cursor position
-
-        // Format the SSN
-        const formattedSSN = formatSSN(ssnInput.value);
-        ssnInput.value = formattedSSN;
-
-        // Calculate the new cursor position
-        let newCursorPosition;
-        if (cursorPosition < 8) {
-            newCursorPosition = cursorPosition; 
-        } else {
-            newCursorPosition = cursorPosition + 1;
-        }
-        ssnInput.setSelectionRange(newCursorPosition, newCursorPosition);
-    });
 
     // Event listener for Firstname input
     fnameInput.addEventListener('input', function() {
