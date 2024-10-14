7599860ce9949ae03eb013fecbe4250f4e589cfd
Santhosh Rajakumar <140490852+SanthoshRajakumar@users.noreply.github.com>
2024-10-14 12:04:06 +0200

    lol

--- homepage.php
+++ homepage.php
@@ -65,21 +65,12 @@ echo '<h2>Welcome, ' . $row["FirstName"] . '!</h2>';
   <form action="room.php" method="GET">
     <button type="submit" class="button button-large">Rooms</button>
   </form>
-<<<<<<< HEAD
-  <form action="profile_edit.php" method="GET">
-    <button type="submit" class="button button-large">Edit Profile</button>
-  </form>
-  <form action="reset_password.php" method="GET">
-    <button type="submit" class="button button-large">Reset Password</button>
-  </form>
-=======
   <div class="div1">
   <form action = "room_creation/new_room_form.php" method = "GET">
     <button type="submit" class="button button-large">Create Room</button>
      
   </form>
 
->>>>>>> 599cb1cfa5b0ca91f23a95d083881f01cc3d3eb9
 <?php
 
 # Should check for role here, when this is implemented.
