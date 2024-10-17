8f90d91521303586c01d345346e96c5ba8cae162
elsarosenblad <elsa.rosenblads@gmail.com>
2024-10-17 09:43:51 +0200

    .

--- forgot_password.php
+++ forgot_password.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>FAQs</title>
+    <title>Forgot Password</title>
     <link rel="icon" type="images/x-icon" href="/images/PastedGraphic-1.png">
     <!-- Link to your CSS file -->
     <link rel="stylesheet" href="style_.css">
@@ -29,66 +29,27 @@
 <form action="index.php" method="GET">
       <button type="submit" class="button2">HOME</button>
     </form>
-    <form action="about.php" method="GET">
+    <form action="../site_info/about.php" method="GET">
       <button type="submit" class="button2">ABOUT</button>
     </form>
-    <form action="faq.php" method="GET">
+    <form action="../site_info/faq.php" method="GET">
       <button type="submit" class="button2">FAQ</button>
     </form>
-    <form action="contact.php" method="GET">
+    <form action="../site_info/contact.php" method="GET">
       <button type="submit" class="button2">CONTACT</button>
     </form>
 </header>
 
 <div class="div2">
-    <h2> FAQs</h2>
-    <p>Have a question for us? Perhaps you can find the answers below!</p>
+    <h2> Forgot you password? </h2>
 </div>
 
-<div class="div1">
-  <div class="box1">
-    <h3>How do you make the inventory tables? What do you need from me to get started?</h3>
-        <p>We take the list with names of all the shelfs in your inventory (in .csv format) and upload them to our system. 
-        The inventory are then displayed in our system and you can start creating courses and sessions with different access. 
-        We also put in a bit of manual work to make sure the storage is just as you want it!</p>
-    </div>
-    <div class="box1">
-        <h3>What industries do you create the inventory displays for?</h3>
-        <p>We create inventory displays for all educational laboratories; university, healthcare, reseach and more! </p>
-    </div>
-</div>
-
-<div class="div1">
-    <div class="box1">
-    <h3>How much does it cost?</h3>
-        <p>Our pricing is dependant upon the size of your inventory, and also the solutions that you’re interested in implementing. 
-        For a price quote, visit our contact page and get in touch with us. </p>
-    </div>
-    <div class="box1">
-    <h3>How long do the inventory take to set-up?</h3>
-        <p>The time it takes to set-up your inventory depends on which features you wish to implement, your existing stucture of the storage, etc. 
-        The inventory table and amount display on their own are typically straightforward and quick to set-up. 
-        For any additional features you select, this time may increase - but it’s hard to give an exact number as it will be assessed on a case-by-case basis.</p>
-    </div>
-</div>
-
-<div class="div1">
-    <div class="box1">
-    <h3>Can I customize the design of my inventory?</h3>
-        <p>The look and feel of the inventory table as a whole (interface, fonts, etc.) cannot be changed. 
-        However, you can customize the what you want to be written and displayed. Get in touch with us and we will explain more!</p>
-    </div>
-    <div class="box1">
-    <h3>How do students access the lab inventory?</h3>
-        <p>Students are granted access to LabLoGGr through course keys provided by their teachers or TAs. Once they log in, they can view
-        a filtered version of the inventory, displaying only the materials relevant to their coursework.</p>
-    </div>
-</div>
 
 <div class="div1">
 <div class="box1">
-    <h3> Could not find the answers you were looking for?</h3>
-        <p>Do not hesitate to contact us. You find our contact information in the top menu.</p>
+    <h3>Enter your registered email to reset your password</h3>
+    <input type="email" name="user_email" id="user_email" placeholder="Email">
+			<button class="button button-large">Reset Password</button>
     </div>
 </div>
 
