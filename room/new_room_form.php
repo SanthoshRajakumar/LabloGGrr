f7a9500ac682fcd6a6ac810628e3cd6e2243f8d3
Johan5434 <eliassonjohan1@gmail.com>
2024-10-18 11:37:57 +0200

    lool

--- room/new_room_form.php
+++ room/new_room_form.php
@@ -1,6 +1,6 @@
 <?php
 session_start(); 
-include '../dopen.php';
+include '../database/dopen.php';
 
 ?>
 
@@ -46,7 +46,7 @@ include '../dopen.php';
   </header>
 
   <!-- Flytta formuläret inuti denna sektion -->
-  <form action="new_room.php" method="POST">
+  <form action="../backend/new_room.php" method="POST">
     <label for="room_name">Room Name:</label>
     <input type="text" id="room_name" name="room_name" required>
     <button type="submit" class="button2">Submit</button>
