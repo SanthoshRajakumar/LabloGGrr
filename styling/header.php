d417a6dccf0a6f91f75ee1b216fe3892fee4aba1
smosebach <sara_1022@hotmail.com>
2024-10-25 17:52:53 +0200

    dropdown in header

--- styling/header.php
+++ styling/header.php
@@ -26,6 +26,7 @@
 
   <header>
 <?php
+
 if(isset($_SESSION['roleID'])){
     if ($_SESSION['roleID'] === 4){
     echo '<form action="/room/room.php" method="GET">
@@ -51,4 +52,55 @@ if(isset($_SESSION['roleID'])){
     <form action="/site_info/contact.php" method="GET">
       <button type="submit" class="button2">CONTACT</button>
     </form>
+  <?php
+    if(isset($_SESSION['userID'])){
+      echo '<div class="dropdown">
+          <button class="button2">ACCOUNT
+      <i class="fa fa-caret-down"></i>
+    </button>
+    <div class="dropdown-content">
+      <a class="button2" href="/profile/profile_edit.php">Edit profile</a>
+      <a class="button2" href="/profile/reset_password.php">Reset password</a>
+      <a class="button2" href="/logout/logout_form.php">Logout</a>
+    </div>
+  </div>';
+    }
+  
+  ?>
+    
   </header>
+
+  <style>
+    /* The dropdown container */
+.dropdown {
+  float: left;
+  overflow: hidden;
+}
+
+/* Dropdown content (hidden by default) */
+.dropdown-content {
+  display: none;
+  position: absolute;
+  background-color: #f9f9f9;
+  min-width: 16vw;
+  box-shadow: 0vw 0.1vw 1vw 0vw rgba(0,0,0,0.2);
+  z-index: 1;
+}
+
+/* Links inside the dropdown */
+.dropdown-content a {
+  float: none;
+  padding: 1vw 1vw;
+  text-decoration: none;
+  display: block;
+  text-align: left;
+}
+
+/* Add a grey background color to dropdown links on hover */
+
+
+/* Show the dropdown menu on hover */
+.dropdown:hover .dropdown-content {
+  display: block;
+}
+  </style>
